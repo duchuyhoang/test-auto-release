d01074415997da3dbfb8d31b037158ee7fa6e777
Huy Hoang <huyhd2@vmogroup.com>
2024-10-09 09:40:02 +0700

    fix: fix workflow file 5

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,15 +1,11 @@
-import { goi } from "../src";
+import { goi } from '../src';
 
 describe('Test', () => {
+  it('Return 1', async () => {
+    expect(goi()).toBe(1);
+  });
 
-
-    it('Return 1', async () => {
-        expect(goi()).toBe(1);
-    })
-
-    it('Not To be 2', async () => {
-        expect(goi()).not.toBe(2);
-    })
-
-
-})
+  it('Not To be 2', async () => {
+    expect(goi()).not.toBe(2);
+  });
+});
