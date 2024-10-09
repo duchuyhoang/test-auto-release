ee5cd752902b374c5276c8c989d7e7e907fe37c8
Huy Hoang <huyhd2@vmogroup.com>
2024-10-09 09:28:06 +0700

    feat: add running test workflow

---
+++ tests/index.spec.ts
@@ -0,0 +1,15 @@
+import { goi } from "../src";
+
+describe('Test', () => {
+
+
+    it('Return 1', async () => {
+        expect(goi()).toBe(1);
+    })
+
+    it('Not To be 2', async () => {
+        expect(goi()).not.toBe(2);
+    })
+
+
+})
